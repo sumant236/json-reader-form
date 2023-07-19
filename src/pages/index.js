b1ed1be61f4c5f2273a6c504454e1443843ab39d
Sumant236 <sumantsingh236@gmail.com>
2023-07-19 12:06:08 +0530

    completed json reader project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,191 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import LeftArrowIcon from "../images/LeftArrowIcon.svg";
+import EmailIcon from "../images/EmailIcon.svg";
+import FileUploadIcon from "../images/fileUploadIcon.svg";
+import MarkIcon from "../images/MarkIcon.svg";
+import {
+  Button,
+  Divider,
+  Input,
+  Modal,
+  Spin,
+  Typography,
+  Upload,
+  message,
+  Form,
+} from "antd";
+import { useState } from "react";
+
+const { Title, Text } = Typography;
+const { Dragger } = Upload;
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [dataFile, setdataFile] = useState("");
+  const [dataEntries, setDataEntries] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [form] = Form.useForm();
+  // handle the "go to my entries" button of modal
+  const handleOk = (e) => {
+    setOpen(false);
+  };
+
+  // handle the "cancel" button of modal
+  const handleCancel = (e) => {
+    setOpen(false);
+  };
+
+  // check if the file type is json or not
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === "application/json";
+    if (!isJpgOrPng) {
+      message.error("You can only upload Json file!");
+    }
+    return isJpgOrPng;
+  };
+
+  // handle the input change of file change
+  const handleChange = (info) => {
+    const { status, originFileObj } = info.file;
+
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+
+    if (status === "done") {
+      // Read the file data here
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const fileData = e.target.result;
+        setdataFile(fileData);
+      };
+      reader.readAsText(originFileObj);
+      setLoading(false);
+    } else if (status === "error") {
+      // File upload failed
+      message.error("File upload failed");
+      setLoading(false);
+    }
+  };
+
+  // handle submit button
+  const handleSubmit = () => {
+    let dataLength = 0;
+    // calculate data entries
+    if (dataFile) {
+      const data = JSON.parse(dataFile);
+      Object.keys(data).map(
+        (keyName, keyIndex) => (dataLength += data[keyName].length)
+      );
+      setDataEntries(dataLength);
+      setOpen(true);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-[#443C3C]">
+      <div className="max-w-sm mx-h-screen m-auto bg-white">
+        {/* Form */}
+        <Form form={form} layout="vertical">
+          <div className="px-5">
+            <div className="flex gap-5 items-center">
+              <Image src={LeftArrowIcon} alt="left arrow icon" />
+              <Title level={4} style={{ margin: 0 }}>
+                Submit form
+              </Title>
+            </div>
+            <Form.Item label="Full Name" className="mt-4">
+              <Input
+                size="large"
+                placeholder="Full Name"
+                bordered={false}
+                className="bg-[#FAFAFA]"
+              />
+            </Form.Item>
+            <Form.Item label="Email">
+              <Input
+                size="large"
+                placeholder="Email"
+                bordered={false}
+                className="bg-[#FAFAFA]"
+                suffix={<Image src={EmailIcon} alt="email icon" />}
+              />
+            </Form.Item>
+            <Form.Item label="Upload JSON File">
+              <Dragger
+                name="file uploader"
+                showUploadList={false}
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+                className="rounded-2xl h-28 bg-[#FAFAFA]"
+              >
+                <div className="flex items-center flex-col">
+                  {loading ? (
+                    <>
+                      <Spin size="large" />
+                      <Text className="text-[#4381FF]">Validating</Text>
+                    </>
+                  ) : (
+                    <>
+                      <Image src={FileUploadIcon} alt="file upload icon" />
+                      <p className="ant-upload-hint">Browse File</p>
+                    </>
+                  )}
+                </div>
+              </Dragger>
+            </Form.Item>
+            <Form.Item label="File Contents">
+              <pre className="overflow-auto bg-[#FAFAFA] h-36">{dataFile}</pre>
+            </Form.Item>
+          </div>
+          <Divider className="m-0" />
+          <div className="px-5 py-5">
+            <Button
+              className="rounded-3xl bg-[#3062C8] py-3 h-12 text-white"
+              block
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </div>
+        </Form>
+        <Modal
+          open={open}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={[
+            <Button
+              key="submit"
+              onClick={handleCancel}
+              className="bg-[#3062C8] rounded-3xl w-full my-4 text-white"
+            >
+              Go to My Entries
+            </Button>,
+            <Button
+              key="back"
+              type="primary"
+              onClick={handleOk}
+              className="bg-[#E9F0FF] text-[#4381FF] rounded-3xl w-full"
+              style={{ margin: 0 }}
+            >
+              cancel
+            </Button>,
+          ]}
+          closeIcon={false}
+          width={300}
+        >
+          <div className="text-center items-center">
+            <Image src={MarkIcon} className="m-auto" alt="Mark icon" />
+            <Text className="text-[#4381FF]">Success</Text>
+            <div className="my-4">
+              <Text>{`${dataEntries} entries successfully uploaded`}</Text>
+            </div>
+          </div>
+        </Modal>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
